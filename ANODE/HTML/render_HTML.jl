a0a4fb3b33d23c3e103696c283ebeccb41a85376
NicolajHMNielsen <nicolaj-mojn@hotmail.com>
2021-04-09 10:19:27 +0200

    update

--- ANODE/HTML/render_HTML.jl
+++ ANODE/HTML/render_HTML.jl
@@ -1,25 +1,5 @@
 #Nicolaj
 using Weave
-filename = normpath("C:\\Users\\Nicolaj\\Documents\\GitHub\\SciMLDTU\\SciMLDTU\\Week1\\lecture3_Nicolaj.jmd")
 
-weave(filename; doctype = "md2html", out_path = "C:\\Users\\Nicolaj\\Documents\\GitHub\\SciMLDTU\\SciMLDTU\\Week1")
-
-#Anton
-filename = normpath("C:\\Users\\anton\\OneDrive - Københavns Universitet\\Uni\\Uni\\8. semester\\SciML\\SciMLDTU\\SciMLDTU\\Week1\\filename.jmd")
-
-weave(filename; doctype = "md2html", out_path = "C:\\Users\\anton\\OneDrive - Københavns Universitet\\Uni\\Uni\\8. semester\\SciML\\SciMLDTU\\SciMLDTU\\Week1")
-
-
-# augmented
-
-filename = normpath("C:\\Users\\Nicolaj\\Documents\\GitHub\\SciMLDTU\\SciMLDTU\\Augmented\\Augmented.jmd")
-
-weave(filename; doctype = "md2html", out_path = "C:\\Users\\Nicolaj\\Documents\\GitHub\\SciMLDTU\\SciMLDTU\\Augmented\\HTML_Files")
-
-
-# hidden bears
-
-
-filename = normpath("C:\\Users\\Nicolaj\\Documents\\GitHub\\SciMLDTU\\SciMLDTU\\Augmented\\Hidden_bears.jmd")
-
-weave(filename; doctype = "md2html", out_path = "C:\\Users\\Nicolaj\\Documents\\GitHub\\SciMLDTU\\SciMLDTU\\Augmented\\HTML_Files")
+filename_test = normpath(raw".\ANODE\Markdown\testRender.jmd")
+weave(filename_test; doctype = "md2html", out_path = raw".\ANODE\HTML\Test")
