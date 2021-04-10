a19af618ca9cfac6493d119fe4c0e8755b788839
NicolajHMNielsen <nicolaj-mojn@hotmail.com>
2021-04-10 09:26:04 +0200

    Corrections fof gifs

--- ANODE/HTML/render_HTML.jl
+++ ANODE/HTML/render_HTML.jl
@@ -1,5 +1,9 @@
-#Nicolaj
 using Weave
 
+# test
 filename_test = normpath(raw".\ANODE\Markdown\testRender.jmd")
 weave(filename_test; doctype = "md2html", out_path = raw".\ANODE\HTML\Test")
+
+# render introduction to augmented
+filename_test = normpath(raw".\ANODE\Markdown\Augmented.jmd")
+weave(filename_test; doctype = "md2html", out_path = raw".\ANODE\HTML\Augmented")
