fe78010e748587685e619eae61f254172ed5ef0c
NicolajHMNielsen <nicolaj-mojn@hotmail.com>
2021-06-14 12:50:33 +0200

    bears updated

--- ANODE/HTML/render_HTML.jl
+++ ANODE/HTML/render_HTML.jl
@@ -7,3 +7,7 @@ weave(filename_test; doctype = "md2html", out_path = raw".\ANODE\HTML\Test")
 # render introduction to augmented
 filename_test = normpath(raw".\ANODE\Markdown\Augmented.jmd")
 weave(filename_test; doctype = "md2html", out_path = raw".\ANODE\HTML\Augmented")
+
+# render introduction to bears
+filename_bears = normpath(raw".\ANODE\Markdown\Hidden_bears.jmd")
+weave(filename_bears; doctype = "md2html", out_path = raw".\ANODE\HTML\Hidden_bears")
